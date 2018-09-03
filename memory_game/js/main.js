b9fbb75a2dcd09b5d0d4bd65123449d9a32d260d
Alex <climberfields@gmail.com>
2018-09-03 12:28:53 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,28 @@
-console.log("Up and running!");
-
-
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ 
+{ rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png" },
+  {
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-diamonds.png",
+  },
+  { rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png",
+  },
+  { rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",}
+]
+	;
 var cardsInPlay = [];
-var checkForMatch = function() {if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) 
+var checkForMatch = function() {if (cardsInPlay[0] === cardsInPlay[1]) 
  	{alert("You found a match!");
 } else { alert("Sorry, try again.");
 }
-
-var flipCard = function () { cardId
+};
+var flipCard = function (cardId) {
 /* blocking out but if needed later it's here.
 
 console.log("User flipped queen");
@@ -19,7 +33,9 @@ if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
  	{alert("You found a match!");
 } else { alert("Sorry, try again.");
 }
-console.log( "User flipped " + 'cardsId')
+console.log( "User flipped " + cards[cardId].rank)
 };
-cardsInPlay.push(cards[cardId])
-flipcard(0);
+cardsInPlay.push(cards[cardId].rank)
+flipcard(0)
+flipcard(2)
+console.log(cards.cardImage && cards.suit);
